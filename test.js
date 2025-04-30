1b8f4f129231320a35fb7a3d8131ffc86b4cf4ca
samequemarques0 <sameque12@gmail.com>
2025-04-29 22:32:56 -0300

    removido o arquivo test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelectorAll('#btnImprimirMobile');
-btn.addEventListerEnner('click', ()=>{
-    alert('Vamos Imprimir');
-    btn.style.display = "none";
-    document.print();
-})
